161a441b017728ba12c24acccfa7be673793c99a
CodeWithYashp <ue205131.yash.ece@gmail.com>
2023-08-10 12:40:59 +0530

    update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,16 +4,16 @@ export default function Footer() {
   return (
     <div className="footer-container">
       <a href="https://www.twitter.com" target="_blank">
-        <img src="./public/twitter.png" alt="" />
+        <img src="/twitter.png" alt="" />
       </a>
       <a href="https://www.facebook.com" target="_blank">
-        <img src="./public/facebook.png" alt="" />
+        <img src="/facebook.png" alt="" />
       </a>
       <a href="https://www.instagram.com" target="_blank">
-        <img src="./public/instagram.png" alt="" />
+        <img src="/instagram.png" alt="" />
       </a>
       <a href="https://www.github.com" target="_blank">
-        <img src="./public/github.png" alt="" />
+        <img src="./github.png" alt="" />
       </a>
     </div>
   );
